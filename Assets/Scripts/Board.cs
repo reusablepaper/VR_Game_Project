99cdfa68d22f503944e2498c141c9c5f73301d67
homebd <gl4481@naver.com>
2024-05-21 20:39:58 +0900

    #4 신버전 보드 + 펜

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,50 +2,61 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private Camera _textureCamera;
-    [SerializeField] private Shader _shader;
-    [SerializeField] private GameObject _backside;
+    [SerializeField] private Material _baseMaterial;
 
-    private RenderTexture _renderTexture;
-    private Material _material;
+    private GameObject _inputBox;
+    private LineRenderer _line;
+
+    private float _minDistance = 0.015f;
+    private int _inkCount = 40;
+
+    private bool IsFinished => _line.loop;
+
 
     private void Awake()
     {
-        GenerateRenderTexture();
-        GenerateMaterial();
+        _inputBox = new GameObject("Input Box");
+        _inputBox.transform.parent = gameObject.transform;
+        _inputBox.layer = gameObject.layer;
+        _inputBox.AddComponent<BoxCollider>().size = new Vector3(1.5f, 1f, 0.01f);
 
-        _backside.GetComponent<Renderer>().material = _material;
+        _line = gameObject.AddComponent<LineRenderer>();
+
+        _line.material = new Material(_baseMaterial);
+        _line.startWidth = 0.01f;
+        _line.endWidth = 0.01f;
+        _line.alignment = LineAlignment.TransformZ;
+        _line.positionCount = 0;
+
+        _line.material.color = new Color(0, 0, 0, 0);
     }
 
-    private void GenerateRenderTexture()
+    public void Draw(Vector3 pos, Color color)
     {
-        _renderTexture = new RenderTexture(4096, 4096, 1);
+        if (IsFinished) return;
+        if (_line.material.color != color)
+        {
+            _line.material.color = color;
 
-        _renderTexture.filterMode = FilterMode.Point;
-        _renderTexture.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm;
-        _textureCamera.targetTexture = _renderTexture;
+            _line.positionCount = 0;
+        }
+
+        if(_line.positionCount == 0 || (_line.GetPosition(_line.positionCount - 1) - pos).magnitude > _minDistance)
+        {
+            _line.SetPosition(_line.positionCount++, pos);
+        }
+
+        if (_line.positionCount == _inkCount) Finish();
     }
 
-    private void GenerateMaterial()
+    public void Finish()
     {
-        _material = new Material(_shader);
+        _line.loop = true;
+        _inputBox.SetActive(false);
 
-        SetAlpha(0.2f);
-        _material.SetTexture("_Cam_Texture", _renderTexture);
-        _material.SetFloat("_Alpha_Threshold", 0.01f);
-
-        GetComponent<Renderer>().material = _material;
-    }
-
-    public void SetAlpha(float alpha)
-    {
-        _material.SetColor("_Color", new Color(0, 0, 0, alpha));
-    }
-
-    public void FinishDraw()
-    {
-        // 00000000 00000000 00000000 00000000
-        // 1111 color
-        //     1111 11111111 11111111 11111111
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        Mesh mesh = new Mesh();
+        _line.BakeMesh(mesh, Camera.main, false); // Camera.main => Origin.Camera, Error Check... distance = 0.1f ... Error Mesh Overlap
+        meshCollider.sharedMesh = mesh;
     }
 }
