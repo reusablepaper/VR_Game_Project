5cff8b0a1557fa5a78cc61edfe29247de5058178
homebd <gl4481@naver.com>
2024-05-22 21:35:30 +0900

    #4 레벨SO 수정

--- Assets/Scripts/ScriptableObjects/LevelSO.cs
+++ Assets/Scripts/ScriptableObjects/LevelSO.cs
@@ -7,11 +7,14 @@ public class LevelSO : ScriptableObject
     [SerializeField] private int _lv;
     public int Lv => _lv;
 
-    //[SerializeField] public List<Pen> UsablePens;
+    [SerializeField] private GameObject _mapPrefab;
+    [SerializeField] private List<Pen> _usablePens;
+    [SerializeField] private Vector3 _playerOffset;
+    [SerializeField] private Vector3 _ballOffset;
 
-    [SerializeField] private Vector3 _playerInitialPosition;
-    public Vector3 PlayerInitialPosition => _playerInitialPosition;
 
-    [SerializeField] private Vector3 _ballInitialPosition;
-    public Vector3 BallInitialPosition => _ballInitialPosition;
+    public GameObject MapPrefab => _mapPrefab;
+    public List<Pen> UseablePens => _usablePens;
+    public Vector3 PlayerOffset => _playerOffset;
+    public Vector3 BallOffset => _ballOffset;
 }
