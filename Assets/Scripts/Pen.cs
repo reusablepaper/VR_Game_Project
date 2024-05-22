1ed5c681a0ba61ec8950b10633a849988707534c
homebd <gl4481@naver.com>
2024-05-22 21:26:29 +0900

    #4 펜, 볼 로직 수정

--- Assets/Scripts/Pen.cs
+++ Assets/Scripts/Pen.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class Pen : MonoBehaviour
 {
-    void Update()
+    [SerializeField] private Material _material;
+
+    private void Awake()
+    {
+        _material = new Material(_material);
+        GetComponent<Renderer>().material = _material;
+    }
+
+    private void Update()
     {
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 0.3f, 1 << LayerMask.NameToLayer("Board")))
         {
-            hit.transform.parent.GetComponent<Board>().Draw(transform.position, Color.red);
+            hit.transform.parent.GetComponent<Board>().Draw(transform.position, _material.color);
         }
+    }
+
+    public void SetColor(Color color)
+    {
+        _material.color = color;
     }
 }
