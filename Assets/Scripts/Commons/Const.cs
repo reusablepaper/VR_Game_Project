0b15416dd34f05fc8c5e22ad4f0ff3e3a44ef34f
homebd <gl4481@naver.com>
2024-05-25 00:03:29 +0900

    [#10] Resource 매니저 추가
    경로를 Const.cs 파일에 const string으로 저장

---
+++ Assets/Scripts/Commons/Const.cs
@@ -0,0 +1,10 @@
+public abstract class Const
+{
+
+    public const string Prefabs_Board = "Prefabs/Draw/Board";
+    public const string Prefabs_Levels = "Prefabs/Levels/";
+    public const string Prefabs_Pen = "Prefabs/Pen";
+    public const string Prefabs_Ball = "Prefabs/Ball";
+
+    public const string ScriptableObjects_Levels = "ScriptableObjects/Levels/";
+}
