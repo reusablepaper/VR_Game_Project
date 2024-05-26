a38711ac9ac97315a7521006334f3c1b97a09f49
homebd <gl4481@naver.com>
2024-05-26 20:11:47 +0900

    [#10] Pen 업데이트문을 코루틴으로 대체

--- Assets/Scripts/Objects/Pen.cs
+++ Assets/Scripts/Objects/Pen.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Pen : MonoBehaviour
 {
     private Material _material;
+    private LevelController _levelContoller;
+
     private Vector3 _originalPosition;
     private Quaternion _originalRotation;
-    private LevelController _levelContoller;
+
+    private WaitForSeconds _raycastTerm = new WaitForSeconds(0.1f);
 
     private void Awake()
     {
@@ -14,20 +18,16 @@ public class Pen : MonoBehaviour
         _material = new Material(r.material);
         r.material = _material;
 
-        XRGrabInteractable _grabInteractable = GetComponent<XRGrabInteractable>();
-        _grabInteractable.selectExited.AddListener((SelectExitEventArgs args) => transform.SetLocalPositionAndRotation(_originalPosition, _originalRotation));
-        _grabInteractable.activated.AddListener((ActivateEventArgs args) => {
-            _grabInteractable.deactivated.RemoveAllListeners(); // T.T
-            _levelContoller.BoardSpawner.SpawnBoard(this);
-            });
-    }
-
-    private void Update()
-    {
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 0.3f, 1 << LayerMask.NameToLayer("Board")))
+        XRGrabInteractable _grab = GetComponent<XRGrabInteractable>();
+        _grab.selectExited.AddListener((SelectExitEventArgs args) => transform.SetLocalPositionAndRotation(_originalPosition, _originalRotation));
+        _grab.activated.AddListener((ActivateEventArgs args) =>
         {
-            hit.transform.parent.GetComponent<Board>().Draw(transform.position + transform.forward * Vector3.Distance(transform.position, hit.point), _material.color);
-        }
+            _grab.deactivated.RemoveAllListeners();
+            _levelContoller.BoardSpawner.SpawnBoard(this);
+            StartCoroutine(nameof(RaycastForDrawing));
+            _grab.deactivated.AddListener((DeactivateEventArgs args) => StopCoroutine(nameof(RaycastForDrawing)));
+        });
+        
     }
 
     public void Init(LevelController levelController, PenColor color)
@@ -42,7 +42,7 @@ public class Pen : MonoBehaviour
         {
             PenColor.Black => Color.black,
             PenColor.Blue => Color.blue,
-            PenColor.LightGreen => new Color(0.3f, 1f, 0f, 1f),
+            PenColor.LightGreen => new Color(0.7f, 1f, 0.2f, 1f),
             PenColor.Green => Color.green,
             PenColor.Yellow => Color.yellow,
             PenColor.Orange => new Color(1f, 1f, 0f, 1f),
@@ -50,5 +50,22 @@ public class Pen : MonoBehaviour
             PenColor.Purple => new Color(1f, 0f, 1f, 1f),
             _ => Color.clear
         };
+    }
+
+    private IEnumerator RaycastForDrawing()
+    {
+        Board board;
+
+        while(true)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 0.3f, 1 << LayerMask.NameToLayer("Board")))
+            {
+                if (hit.transform.parent.TryGetComponent(out board))
+                {
+                    board.Draw(hit.point, _material.color);
+                }
+            }
+            yield return _raycastTerm;
+        }
     }
 }
